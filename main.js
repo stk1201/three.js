0e1ae5ecc080a4e3efe9abfaefec40e26807f1a0
stk1201 <143611554+stk1201@users.noreply.github.com>
2024-04-03 21:09:44 +0900

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
-import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
